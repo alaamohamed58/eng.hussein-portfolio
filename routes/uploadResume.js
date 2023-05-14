24c3ce48e662100c1382481c12eabd2ebfca4816
alaamohamed58 <alaamohamed58999@gmail.com>
2023-05-14 12:40:12 -0700

    upload cv

--- routes/uploadResume.js
+++ routes/uploadResume.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
 const express = require("express");
 
-const { uploadResume, upload } = require("../controllers/uploadResume");
+const { uploadResume, upload, getCV } = require("../controllers/uploadResume");
 
 const router = express.Router();
 
-router.post("/", upload.single("file"), uploadResume);
+router.route("/").post(upload.single("file"), uploadResume).get(getCV);
 
-router.get("/get-cv", (req, res) => {
-  const filePath = fs// Send the CV file as a response
-  .req
-    .status(200)
-    .json({
-      file: filePath,
-    });
-});
+// router.get("/get-cv", (req, res) => {
+//   const filePath = fs.readFile(`${__dirname}/uploads`);
+//   console.log(filePath);
+//   req.status(200).json({
+//     file: filePath,
+//   });
+// });
 
 module.exports = router;
