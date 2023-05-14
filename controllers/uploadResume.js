1a787adf94766920e495a11ca06f6912f5b054cd
alaamohamed58 <alaamohamed58999@gmail.com>
2023-05-14 12:53:20 -0700

    repair

--- controllers/uploadResume.js
+++ controllers/uploadResume.js
@@ -1,77 +1,59 @@
+const path = require("path");
 const multer = require("multer");
 const catchAsync = require("../utils/catchAsync");
-const path = require("path");
 
 const storage = multer.diskStorage({
-  // Specify the destination folder
   destination: "uploads/",
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, "cv.pdf"); // Specify a fixed filename for the CV file
   },
 });
-
-const fileFilter = (req, file, cb) => {
-  // Define the allowed file types
-  const allowedMimeTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/gif",
-    // Add more allowed file types if needed
-  ];
-
-  if (allowedMimeTypes.includes(file.mimetype)) {
-    // Accept the file if its MIME type is in the allowed list
-    cb(null, true);
-  } else {
-    // Reject the file if its MIME type is not allowed
-    cb(
-      new Error(
-        "Invalid file type. Only JPEG, PNG, and GIF images are allowed."
-      )
-    );
-  }
-};
 
 exports.upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
-    const allowedMimeTypes = [
-      "application/pdf",
-      "application/msword",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // Add the MIME type for DOCX files
-    ];
-
-    if (allowedMimeTypes.includes(file.mimetype)) {
+    if (
+      file.mimetype === "application/pdf" ||
+      file.mimetype === "application/msword" ||
+      file.mimetype ===
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ) {
       cb(null, true);
     } else {
       cb(
-        new Error(
-          "Invalid file type. Only PDF, Word documents, and DOCX files are allowed."
-        )
+        new Error("Invalid file type. Only PDF or Word documents are allowed.")
       );
     }
   },
 });
 
-exports.uploadImages = catchAsync(async (req, res) => {
-  // Access the uploaded images using req.files
-  const uploadedImages = req.files;
+exports.uploadResume = catchAsync(async (req, res) => {
+  // Delete the old file if it exists
 
-  if (!uploadedImages || uploadedImages.length === 0) {
-    // Handle the case when no images were uploaded
-    return res.status(400).json({ error: "No images uploaded" });
+  // Access the uploaded file using req.file
+  const uploadedFile = req.file;
+
+  if (!uploadedFile) {
+    // Handle the case when no file was uploaded
+    return res.status(400).json({ error: "No file uploaded" });
   }
 
-  // Process the uploaded images as needed
-  // ...
+  // Process the file as per your requirements
+  // For example, you can obtain the file name, size, and path:
+  const fileName = uploadedFile.originalname;
+  const fileSize = uploadedFile.size;
+  const filePath = uploadedFile.path;
+
+  // Perform additional operations with the file, such as saving it to a database or cloud storage,
+  // or manipulating its content.
 
   // Send a success response
   res.status(201).json({
-    message: "Uploaded images successfully",
-    uploadedImages,
+    message: "Uploaded file successfully",
+    uploadedFile,
   });
 });
+
 exports.getCV = (req, res) => {
   // Specify the path to the CV file
   const filePath = path.join(__dirname, "../uploads/cv.pdf");
